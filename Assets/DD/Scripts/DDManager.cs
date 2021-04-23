f4df29504e3190e823b0e68f0095d11f06782cd6
Sawyer Knowles <sdknowle@mtu.edu>
2021-04-23 16:40:35 -0400

    Added encounter switching, changed beholder location

--- Assets/DD/Scripts/DDManager.cs
+++ Assets/DD/Scripts/DDManager.cs
@@ -9,8 +9,12 @@ public class DDManager : MonoBehaviour
 
     public GameObject rig;
     public GameObject[] spawnPoints;
+    private int count = 0;
     private XRController controller;
-    //private bool pressed;
+    private bool flag = true;
+    private int p = 0;
+    private bool pressedCheck = false;
+    
 
 
     private void Awake()
@@ -21,14 +25,47 @@ public class DDManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig.transform.position = spawnPoints[count].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool pressed)) {
-            Debug.Log("Button Pressed");
+        /*if (pressedCheck == false) {
+            flag = true;
+        }*/
+        controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool pressed);
+        if (pressed && flag) {
+            flag = false;
+            Debug.Log(p);
+            p++;
+            gotoNextEncounter();
+            StartCoroutine(waitCoroutine());
+            //pressed = false;
+
         }
     }
+
+    void gotoNextEncounter() {
+        count++;
+        if (count >= spawnPoints.Length)
+            count = 0;
+        rig.transform.position = spawnPoints[count].transform.position;
+        rig.transform.rotation = spawnPoints[count].transform.rotation;
+    }
+
+    
+    IEnumerator waitCoroutine()
+    {
+        //Print the time of when the function is first called.
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(3);
+        flag = true;
+
+        //After we have waited 5 seconds print the time again.
+        //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+    
 }
